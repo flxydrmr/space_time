fa6718e3f4d4938332bbf032c9049e45ffe898c0
flxydrmr <magneto.electrk@gmail.com>
2020-04-11 14:06:08 +0530

    init

---
+++ case_magnet/cmag_tests/buffer.swift
@@ -0,0 +1,26 @@
+//
+//  buffer.swift
+//  case_magnetTests
+//
+//  Created by Node Writer on 11/04/20.
+//  Copyright © 2020 Node Writer. All rights reserved.
+//
+
+import XCTest
+
+class buffer: XCTestCase {
+    
+    override func tearDown() {}
+    override func setUp() {}
+    
+    func testExample() {
+        
+        //..
+        
+    }
+    
+    func testPerformanceExample() {
+        measure {}
+    }
+}
+
---
+++ case_magnet/cmag_tests/case_magnet.swift
@@ -0,0 +1,27 @@
+//
+//  case_magnetTests.swift
+//  case_magnetTests
+//
+//  Created by Node Writer on 11/04/20.
+//  Copyright © 2020 Node Writer. All rights reserved.
+//
+
+import XCTest
+@testable import case_magnet
+
+class case_magnet: XCTestCase {
+    
+    override func tearDown() {}
+    override func setUp() {}
+    
+    func testExample() {
+        
+        //..
+        
+    }
+    
+    func testPerformanceExample() {
+        measure {}
+    }
+}
+
