78f943ca7fb409b625f40404ebaf89496150318c
flxydrmr <magneto.electrk@gmail.com>
2020-04-11 13:11:02 +0530

    init

---
+++ case_magnet/case_magnet/case_magnet.h
@@ -0,0 +1,19 @@
+//
+//  case_magnet.h
+//  case_magnet
+//
+//  Created by Node Writer on 11/04/20.
+//  Copyright © 2020 Node Writer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for case_magnet.
+FOUNDATION_EXPORT double case_magnetVersionNumber;
+
+//! Project version string for case_magnet.
+FOUNDATION_EXPORT const unsigned char case_magnetVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <case_magnet/PublicHeader.h>
+
+
